9cc2952be3decb905da0ebd98aaee35a5472a922
Build System <builder@pibuild.int.nonexiste.net>
2021-11-18 13:35:46 +0000

    update git repo

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 cd ~/vyos-pi-builder
+
+git pull
+
 sudo docker run -it --privileged -v "$(pwd)":/vyos -v /dev:/dev -w /vyos --sysctl net.ipv6.conf.lo.disable_ipv6=0 vyos/vyos-build:current-arm64 sudo bash -x build-image.sh
